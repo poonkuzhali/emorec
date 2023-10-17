0f27ff37aad1118eb9acb298a51f47dbef5749ce
Termi7 <90416021+Termi7@users.noreply.github.com>
2023-10-17 11:29:31 -0400

    added login page

--- front-end-folder/front-end/src/App.tsx
+++ front-end-folder/front-end/src/App.tsx
@@ -1,26 +1,10 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+// src/UserInput.tsx
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Login from "./components/login/login";
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
+
+function App(): JSX.Element {
+  return <Login></Login>;
 }
 
 export default App;
