231fb25f41743d5a64086a5bedccb5bf350ffa51
Termi7 <90416021+Termi7@users.noreply.github.com>
2023-10-17 17:35:02 -0400

    started chat page

--- front-end-folder/front-end/src/App.tsx
+++ front-end-folder/front-end/src/App.tsx
@@ -1,10 +1,12 @@
 // src/UserInput.tsx
 
+import Chat from "./components/chat/chat";
 import Login from "./components/login/login";
 import "mdb-react-ui-kit/dist/css/mdb.min.css";
 
 function App(): JSX.Element {
-  return <Login></Login>;
+  return <Chat></Chat>;
+  // <Login></Login>;
 }
 
 export default App;
